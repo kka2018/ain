4887edd51bc5c7fda534732c854bd717c5e24df3
kishan <kishan.kushwaha11@gmail.com>
2018-10-07 22:00:24 +0530

    Draw Hangman

---
+++ src/test/DrawHangman.java
@@ -0,0 +1,14 @@
+package test;
+
+public class DrawHangman {
+    public static void main(String[] args){
+         System.out.print(" _ _ _ _ _\n"+
+         "|        |\n"+
+         "|        O\n"+
+         "|      / | \\ \n"+
+         "|        |\n"+
+         "|       / \\ \n"+
+         "|\n" +
+         "|\n");
+    }
+}
